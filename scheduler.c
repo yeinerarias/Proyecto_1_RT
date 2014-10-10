7f40a13c0f712c3319c4d3cbdf3daed90ece48f8
ebridge-vibración <ebridge-vibracion@ebridgevibracion.(none)>
2014-10-10 14:49:13 -0600

    nada interante

--- scheduler.c
+++ scheduler.c
@@ -1,45 +1,114 @@
 #include <stdio.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <math.h>
 
-jmp_buf env; 
+#include "scheduler.h"
 
-static void signal_handler();
 
-int main()
-{
-	 int returned_from_longjump, processing = 1;
-	 unsigned int time_interval = 1;
-	 returned_from_longjump = sigsetjmp(env,1);
-		 switch (returned_from_longjump)     {
-		   case SIGINT:
-			 printf("longjumped from interrupt %d\n",SIGINT);
-			 break;
-		   case SIGALRM:
-			 printf("longjumped from alarm %d\n",SIGALRM);
-			 break;
-		 }
-	 signal(SIGINT, signal_handler);
-	 signal(SIGALRM, signal_handler);
-	 alarm(time_interval);
-	 while (processing)        {
-	   printf(" waiting for you to INTERRUPT (cntrl-C) ...\n");
-	   sleep(1);
-	 }       /* end while forever loop */
-	 return 1;
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+void scheduler(){ 
+	int i;
+    system("clear");
+    printf("Act  Hilo    Aprox                          Trabajo terminado\n");
+    printf("--- ------- --------   ---------------------------------------------------------\n");
+    if(r == 0) {printf("->  Hilo 1: %f   ",(1+acc_h1)*2.0);}
+	else {printf("    Hilo 1: %f   ",(1+acc_h1)*2.0);}
+	for(i = 0; i < act_load_per1;i++)
+	{printf("|");}
+	printf("\n");
+	if(r == 1) {printf("->  Hilo 2: %f   ",(1+acc_h2)*2.0);}
+	else {printf("    Hilo 2: %f   ",(1+acc_h2)*2.0);}
+	for(i = 0; i < act_load_per2;i++)
+	{printf("|");}
+	printf("\n");
+	if(r == 2) {printf("->  Hilo 3: %f   ",(1+acc_h3)*2.0);}
+	else {printf("    Hilo 3: %f   ",(1+acc_h3)*2.0);}
+	for(i = 0; i < act_load_per3;i++)
+	{printf("|");}
+	printf("\n");
+	if(r == 3) {printf("->  Hilo 4: %f   ",(1+acc_h4)*2.0);}
+	else {printf("    Hilo 4: %f   ",(1+acc_h4)*2.0);}
+	for(i = 0; i < act_load_per4;i++)
+	{printf("|");}
+	printf("\n");
+	if(r == 4) {printf("->  Hilo 5: %f   ",(1+acc_h5)*2.0);}
+	else {printf("    Hilo 5: %f   ",(1+acc_h5)*2.0);}
+	for(i = 0; i < act_load_per5;i++)
+	{printf("|");}
+	printf("\n");
+
+    if(r != -1)
+    {
+		if(end_h1 || end_h2 || end_h3 || end_h4 || end_h5){
+			switch (r) {
+				case 0:
+					end_h1 = 0;
+				case 1: 
+					end_h2 = 0;
+				case 2 : 
+					end_h3 = 0;
+				case 3: 
+					end_h4 = 0;
+				case 4: 
+					end_h5 = 0;
+			}
+			tickets[r] = 0;
+			if(count_deads == 4){exit(0);}
+			count_deads += 1;
+			//printf("Alguien Murio \n");
+		}
+	}
+	if (sigsetjmp(env_struct[r],1) == 1) {
+		return;
+	}
+		
+    r = Lottery_Scheduler(tickets);
+	siglongjmp(env_struct[r],1);
+    
 }
 
-static void signal_handler(int sig)
-{
-	 switch (sig)     {
-	   case SIGINT:
-			   siglongjmp(env,sig);
-							 /* break never reached */
-	   case SIGALRM:  
-					  
-			siglongjmp(env,sig);
-							/* break never reached */
 
-	 }
+
+void setuptimer()
+{
+	struct sigaction sa;
+	struct itimerval timer;
+
+	memset (&sa, 0, sizeof (sa));
+	sa.sa_handler = &scheduler;
+	sigaction (SIGALRM, &sa, NULL);
+
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = quantum*1000;
+	// ... and every 250 msec after that.
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = quantum*1000;
+	// Start a real timer. It counts down whenever this process is executing. 
+	setitimer (ITIMER_REAL, &timer, NULL);
+}
+
+
+int main()
+{	
+	srand(time(NULL));
+    crear_hilo();	
+    if(mode)
+    {
+		signal(SIGUSR1,&scheduler);
+    }
+    else
+    {
+		setuptimer();	
+	}
+	config();
+	sleep(1);
+    scheduler();
+    //return 0;
 }
--- scheduler.h
+++ scheduler.h
@@ -1,3 +1,71 @@
+////////////////////////////
+
+float acc_h1,acc_h2,acc_h3,acc_h4,acc_h5;
+int r = -1;
+int count_deads = 0;
+
+int end_h1,end_h2,end_h3,end_h4,end_h5;
+pid_t pid1;
+int load_per1 = 10, load_per2 = 20, load_per3 = 11, load_per4 = 10, load_per5 = 21;
+int n_1=30000, n_2=10000, n_3=10000, n_4=10000, n_5=10000; 
+int quantum = 100;
+int mode = 1;
+int act_load_per1, act_load_per2, act_load_per3, act_load_per4 = 0, act_load_per5;
+int tickets[5];
+
+////////////////////////////////////////////////////////////////////////
+#define SECOND 800000
+#define STACK_SIZE 40096
+
+char stack1[STACK_SIZE];
+char stack2[STACK_SIZE];
+char stack3[STACK_SIZE];
+char stack4[STACK_SIZE];
+char stack5[STACK_SIZE];
+
+////////////////////////////////////////////////////////////////////////
+
+sigjmp_buf env_struct[5];
+
+////////////////////////////////////////////////////////////////////////
+
+typedef unsigned long address_t;
+
+#ifdef __x86_64__
+// code for 64 bit Intel arch
+
+#define JB_SP 6
+#define JB_PC 7
+
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+                "rol    $0x11,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+#else
+// code for 32 bit Intel arch
+#define JB_SP 4
+#define JB_PC 5
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+                "rol    $0x9,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+
+#endif
+
+
+////////////////////////////////////////////////////////////////////////
+
 int Lottery_Scheduler(int tickets[5])
 {
 	int winner;
@@ -19,3 +87,199 @@ int Lottery_Scheduler(int tickets[5])
 	}
 	return 0;
 }
+
+void hilo1( void )
+{
+    int i=0;
+	float j=0;
+	long double acc=1.0;
+	acc_h1=0.0;
+
+	if (end_h1==0){
+		for(i=1;i<n_1+1;i++){
+			acc=1.0;
+			for (j=2*i;j>i;j--){
+				acc*=pow(j,3)/(pow(j,2)*4*(j-i));
+			}
+			act_load_per1 = i*57/n_1;
+			acc=acc/(2*i+1);
+			acc_h1=acc_h1+acc;
+			if (((i % (int)(n_1*load_per1/(100)) == 0) || (n_1 == i)) && (mode)){
+				if(i == n_1){end_h1 = 1;}
+				raise(SIGUSR1);
+            }
+		}
+		end_h1=1;  
+	}    
+	while(1);
+}
+
+void hilo2( void )
+{
+    int  i=0;
+	float j=0;
+	long double acc=1.0;
+	acc_h2=0.0;
+
+	if (end_h2==0){
+		for(i=1;i<n_2+1;i++){
+			acc=1.0;
+			for (j=2*i;j>i;j--){
+				acc*=pow(j,3)/(pow(j,2)*4*(j-i));
+			}
+			act_load_per2 = i*57/n_2;
+			acc=acc/(2*i+1);
+			acc_h2=acc_h2+acc;
+			if (((i % (int)(n_2*load_per2/(100)) == 0) || (n_2 == i)) && (mode)){
+				if(i == n_2){end_h2 = 1;}
+				raise(SIGUSR1);
+            }
+		}
+		end_h2=1;  
+	}    
+	while(1);
+}
+
+void hilo3( void )
+{
+    int  i=0;
+	float j=0;
+	long double acc=1.0;
+	acc_h3=0.0;
+
+	if (end_h3==0){
+		for(i=1;i<n_3+1;i++){
+			acc=1.0;
+			for (j=2*i;j>i;j--){
+				acc*=pow(j,3)/(pow(j,2)*4*(j-i));
+			}
+			act_load_per3 = i*57/n_3;
+			acc=acc/(2*i+1);
+			acc_h3=acc_h3+acc;
+			if (((i % (int)(n_3*load_per3/(100)) == 0) || (n_3 == i)) && (mode)){
+				if(i == n_3){end_h1 = 3;}
+				raise(SIGUSR1);
+            }
+		}
+		end_h3=1;  
+	}   
+	while(1);
+}
+
+void hilo4( void )
+{
+    int  i=0;
+	float j=0;
+	long double acc=1.0;
+	acc_h4=0.0;
+
+	if (end_h4==0){
+		for(i=1;i<n_4+1;i++){
+			acc=1.0;
+			for (j=2*i;j>i;j--){
+				acc*=pow(j,3)/(pow(j,2)*4*(j-i));
+			}
+			act_load_per4 = i*57/n_4;
+			acc=acc/(2*i+1);
+			acc_h4=acc_h4+acc;
+			if (((i % (int)(n_4*load_per4/(100)) == 0) || (n_4 == i)) && (mode)){
+				if(i == n_4){end_h4 = 1;}
+				raise(SIGUSR1);
+            }
+		}
+		end_h4=1;  
+	}   
+	while(1);
+}
+
+void hilo5( void )
+{
+    int  i=0;
+	float j=0;
+	long double acc=1.0;
+	acc_h5=0.0;
+
+	if (end_h5==0){
+		for(i=1;i<n_5+1;i++){
+			acc=1.0;
+			for (j=2*i;j>i;j--){
+				acc*=pow(j,3)/(pow(j,2)*4*(j-i));
+			}
+			act_load_per5 = i*57/n_5;
+			acc=acc/(2*i+1);
+			acc_h5=acc_h5+acc;
+			if (((i % (int)(n_5*load_per5/(100)) == 0) || (n_5 == i)) && (mode)){
+				if(i == n_5){end_h5 = 1;}
+				raise(SIGUSR1);
+            }
+		}
+		end_h5=1;  
+	}   
+	while(1);
+}
+ 
+void crear_hilo()
+{
+    address_t sp, pc;
+    
+    sp = (address_t)stack1 + STACK_SIZE;
+    //sp = malloc(STACK_SIZE) + STACK_SIZE;
+    pc = (address_t)hilo1; 
+    
+    sigsetjmp(env_struct[0],1);
+    (env_struct[0]-> __jmpbuf)[JB_SP] = translate_address(sp);
+    (env_struct[0]-> __jmpbuf)[JB_PC] = translate_address(pc);
+    //(env_struct[0]-> __jmpbuf)[8] = translate_address(pc);
+    //(env_struct[0]-> __jmpbuf)[JB_PC] = translate_address(pc);
+    //sigemptyset(&env_struct[0]-> __saved_mask);     
+
+    sp = (address_t)stack2 + STACK_SIZE ;
+    //sp = malloc(STACK_SIZE) + STACK_SIZE;
+    pc = (address_t)hilo2;
+ 
+    sigsetjmp(env_struct[1],1);
+    (env_struct[1]-> __jmpbuf)[JB_SP] = translate_address(sp);
+    (env_struct[1]-> __jmpbuf)[JB_PC] = translate_address(pc);
+    //sigemptyset(&env_struct[1]-> __saved_mask);    
+    
+    sp = (address_t)stack3 + STACK_SIZE ;
+    pc = (address_t)hilo3;
+ 
+    sigsetjmp(env_struct[2],1);
+    (env_struct[2]-> __jmpbuf)[JB_SP] = translate_address(sp);
+    (env_struct[2]-> __jmpbuf)[JB_PC] = translate_address(pc);   
+    
+    sp = (address_t)stack4 + STACK_SIZE ;
+    pc = (address_t)hilo4;
+ 
+    sigsetjmp(env_struct[3],1);
+    (env_struct[3]-> __jmpbuf)[JB_SP] = translate_address(sp);
+    (env_struct[3]-> __jmpbuf)[JB_PC] = translate_address(pc);   
+    
+    sp = (address_t)stack5 + STACK_SIZE ;
+    pc = (address_t)hilo5;
+ 
+    sigsetjmp(env_struct[4],1);
+    (env_struct[4]-> __jmpbuf)[JB_SP] = translate_address(sp);
+    (env_struct[4]-> __jmpbuf)[JB_PC] = translate_address(pc);     
+}
+
+
+void config()
+{
+	tickets[0] = 8000;
+    tickets[1] = 300;
+    tickets[2] = 10;
+    tickets[3] = 300;
+    tickets[4] = 2000;
+    
+    int c;
+	FILE *file;
+	file = fopen("config.txt", "r");
+	if (file) {
+		while ((c = getc(file)) != EOF)
+			putchar(c);
+		fclose(file);
+}
+}
+
